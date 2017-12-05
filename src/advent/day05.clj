8d3471fe1e88440e6018ccf9ede8eed3e53c5a44
Arne Brasseur <arne@arnebrasseur.net>
2017-12-05 09:05:38 +0100

    Tried to speed it up

--- src/advent/day05.clj
+++ src/advent/day05.clj
@@ -19,6 +19,8 @@
            (update input index inc))))
 
 (defn solve-2 [steps index input]
+  #_(when (= (mod steps 10000) 0)
+      (prn [:vec steps index]))
   (if (out-of-bounds? index input)
     steps
     (let [offset (get input index)]
@@ -26,8 +28,61 @@
              (+ index offset)
              (update input index (if (>= offset 3) dec inc))))))
 
-(solve-2 0 0 [0 3 0 1 -3])                     ;;=> 10
-(solve-2 0 0 input)                            ;;=> 28707598
+;; Java array version of solve-2
 
-(time
- (solve-2 0 0 input))                          ;;=> 28707598
+(defn solve-array [steps index input limit]
+  #_(when (= (mod steps 10000) 0)
+      (prn [:arr steps index]))
+  (if (not (< -1 index limit))
+    steps
+    (let [offset (aget input index)
+          new-offset (if (>= offset 3)
+                       (dec offset)
+                       (inc offset))]
+      (aset input index new-offset)
+      (recur (inc steps)
+             (+ index offset)
+             input
+             limit))))
+
+;; Java array version that avoids boxing/unboxing and reflection
+
+(defn aget-long ^long [arr ^long idx]
+  (. java.lang.reflect.Array (get arr idx)))
+
+(defn aset-long [arr ^long idx ^long val]
+  (. java.lang.reflect.Array (set arr idx val)))
+
+(defn update-offset ^long [^long offset]
+  (if (>= offset 3)
+    (dec offset)
+    (inc offset)))
+
+(defn solve-array-fast [^long steps ^long index input ^long limit]
+  #_(when (= (mod steps 10000) 0)
+      (prn [:arr steps index]))
+  (if (not (< -1 index limit))
+    steps
+    (let [offset (aget-long input index)
+          new-offset (update-offset offset)]
+      (aset-long input index new-offset)
+      (recur (inc steps)
+             (+ index offset)
+             input
+             limit))))
+
+(comment
+  (solve-2 0 0 [0 3 0 1 -3])                     ;;=> 10
+
+  (solve-2 0 0 input)                            ;;=> 28707598
+  ;; "Elapsed time: 21478.656249 msecs"
+
+  (solve-array 0 0 (into-array Long/TYPE input) (count input))
+  ;; This seems to be about 38 times slower than the vector version
+  ;; maybe boxing/unboxing is to blame?
+
+  (solve-array-fast 0 0 (into-array Long/TYPE input) (count input))
+  ;;"Elapsed time: 7459.838384 msecs"
+  ;; Now we're getting somewhere, about 3x faster than the pure Clojure version,
+  ;; by avoiding boxing and reflection.
+  )
