2b8d8d6a9581490a9d0de52ef6247202f34cce84
Arne Brasseur <arne@arnebrasseur.net>
2018-12-04 10:42:22 +0100

    Day 4

--- src/advent2018/util.clj
+++ src/advent2018/util.clj
@@ -1,5 +1,38 @@
 (ns advent2018.util
-  (:import [java.io BufferedReader StringReader]))
+  (:import [java.io BufferedReader StringReader])
+  (:require [clojure.java.io :as io]))
 
 (defn str-line-seq [input]
   (line-seq (BufferedReader. (StringReader. input))))
+
+(defn lines-reducible [^URL resource]
+  (reify clojure.lang.IReduceInit
+    (reduce [this f init]
+      (let [rdr (io/reader resource)]
+        (try
+          (loop [state init]
+            (if (reduced? state)
+              @state
+              (if-let [line (.readLine rdr)]
+                (recur (f state line))
+                state)))
+          (finally
+            (.close rdr)))))
+
+    java.lang.Iterable
+    (iterator [this]
+      (let [reader (io/reader resource)
+            next-line (volatile! nil)]
+        (reify java.util.Iterator
+          (hasNext [this]
+            (when-not @next-line
+              (vreset! next-line (.readLine rdr)))
+            (boolean @next-line))
+          (next [this]
+            (when (.hasNext this)
+              (let [line @next-line]
+                (vreset! next-line nil)
+                line))))))))
+
+(defn resource-reducible [path]
+  (lines-reducible (io/resource path)))
